e616258c13153b9fb5408b9c4bb974bad2ef3f73
Masashi Motohashi <masashi6225@motohashisusumushi-no-MacBook-Pro.local>
2017-04-24 22:30:13 +0900

    second kadai app revised

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Masashi Motohashi on 2017/04/22.
+//  Copyright © 2017 masashi.motohashi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var label:UILabel!
+    
+    var name:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = "こんにちは、\(name)さん"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -19,7 +19,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているtextに文字を代入して渡す
+        resultViewController.name = ""
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        // 他の画面から segue を使って戻ってきた時に呼ばれる
+    }
 }
 
